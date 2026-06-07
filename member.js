8b087138513156982db62e788a69544a996a376f
Matteo <caretti.matteo@gmail.com>
2026-06-07 11:18:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillMember() {
+    return {
+        name: 'skillMember',
+        template: `
+            <div class="skill-member">
+                <h3>Skill Member</h3>
+                <p>This is the skill member component.</p>
+            </div>
+        `
+    };
+}
